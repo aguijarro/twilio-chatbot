84bc9985473459573bf8f487e2ce09aee41f25f7
aguijarro <ec.andres.guijarro@gmail.com>
2022-01-08 13:25:55 -0500

    Add whatsapp_webhook function

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
-# This is a sample Python script.
+import logging
+import requests
+from twilio.twiml.messaging_response import MessagingResponse
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+logger = logging.getLogger(__name__)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def whatsapp_webhook(request):
+    """HTTP Cloud Function.
+    Parameters
+    ----------
+    request (flask.Request) : The request object.
+        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
+    Returns
+    -------
+        The response text, or any set of values that can be turned into a
+        Response object using `make_response`
+        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
+    """
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    country = request.values.get('Body', "").lower()
+    resp = requests.get(f'https://restcountries.eu/rest/v2/name/{country}?fullText=true')
+    twilio_response = MessagingResponse()
+    msg = twilio_response.message()
+    if not (200 <= resp.status_code <= 299):
+        logger.error(
+            f'Failed to retrieve data for the following country - {country.title()}. Here is a more verbose reason {resp.reason}'
+        )
+        msg.body(
+            'Sorry we could not process your request. Please try again or check a different country'
+        )
+    else:
+        data = resp.json()[0]
+        native_name = data['nativeName']
+        capital = data['capital']
+        people = data['demonym']
+        region = data['region']
+        msg.body(
+            f"{country.title()} is a country in {region}. "
+            f"It's capital city is {capital}, while it's native name is {native_name}. "
+            f"A person from {country.title()} is called a {people}."
+        )
+    return str(twilio_response)
